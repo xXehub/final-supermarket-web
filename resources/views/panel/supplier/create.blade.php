c6ad0c3763116adfaab91fd9a7e4293d28550ba3
badfellas <deruanggoro26@gmail.com>
2024-06-09 03:42:21 +0700

    fix bug supplier create

--- resources/views/panel/supplier/create.blade.php
+++ resources/views/panel/supplier/create.blade.php
@@ -1,1 +1,100 @@
-create
+<form action="{{ route('supplier.store') }}" method="POST" enctype="multipart/form-data" id="formTambahData">
+    @csrf
+    <div class="modal modal-blur fade" id="modal-tambahSupplier" tabindex="-1" role="dialog" aria-hidden="true"
+        data-bs-backdrop="static">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Tambah Data Supplier</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Nama supplier</label>
+                                <input type="text" class="form-control @error('nama_supplier') is-invalid @enderror"
+                                    name="nama_supplier" id="nama_supplier" value="{{ old('nama_supplier') }}"
+                                    placeholder="Masukan Nama barang">
+                                @error('nama_supplier')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Kode Supplier</label>
+                                <input type="text" class="form-control @error('kode_supplier') is-invalid @enderror"
+                                    name="kode_supplier" id="kode_supplier" value="{{ old('kode_supplier') }}" readonly disabled>
+                            </div>
+                        </div>
+                        {{-- alamat --}}
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Alamat</label>
+                                <input type="text" class="form-control @error('alamat') is-invalid @enderror"
+                                    name="alamat" id="alamat" value="{{ old('alamat') }}"
+                                    placeholder="Masukan Alamat">
+                                @error('alamat')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        {{-- no hp --}}
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Telephone</label>
+                                <input type="text" class="form-control @error('no_hp') is-invalid @enderror"
+                                    name="no_hp" id="no_hp" value="{{ old('no_hp') }}"
+                                    placeholder="Masukan Nomor Telephone">
+                                @error('no_hp')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div>
+                                <label class="form-label">Keterangan</label>
+                                <textarea class="form-control @error('deskripsi') is-invalid @enderror" id="deskripsi" rows="3" name="deskripsi"
+                                    placeholder="Produk ini adalah produk terlarang yang nantinya akan di ekspor ke berbagai negara "
+                                    {{ old('deskripsi') }} rows="3"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
+                        Cancel
+                    </a>
+                    <div class="col-auto ms-auto d-print-none">
+                        <div class="btn-list">
+                            <span class="d-none d-sm-inline">
+                                <a class="btn" onclick="resetForm()">
+                                    Reset
+                                </a>
+                            </span>
+                            <button type="submit" class="btn btn-primary d-none d-sm-inline-block">
+                                <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M12 5l0 14" />
+                                    <path d="M5 12l14 0" />
+                                </svg>
+                                Simpan
+                            </button>
+                            <button type="submit" class="btn btn-primary d-sm-none btn-icon"
+                                aria-label="Tambah Produk">
+                                <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M12 5l0 14" />
+                                    <path d="M5 12l14 0" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
