2d397f3c43e69d29a8f2ce1d028b8c1341058b6a
badfellas <deruanggoro26@gmail.com>
2024-06-18 23:24:48 +0700

    fix navbar tidak muncul pada user

--- resources/views/layouts/navbar_user.blade.php
+++ resources/views/layouts/navbar_user.blade.php
@@ -45,12 +45,13 @@
                 </div>
             </div>
         @endguest
-        @role('user|admin|superadmin')
+
+        @auth
             <div class="navbar-nav flex-row order-md-last">
                 <div class="d-none d-md-flex">
                     {{-- gawe notif --}}
                     <div class="nav-item dropdown d-none d-md-flex me-1">
-                        {{-- default e <div class="nav-item dropdown d-none d-md-flex me-3">--}}
+                        {{-- default e <div class="nav-item dropdown d-none d-md-flex me-3"> --}}
                         <a href="{{ route('supermarket.keranjang.index') }}" class="nav-link px-0" tabindex="-1"
                             aria-label="Show notifications">
                             <!-- Download SVG icon from http://tabler-icons.io/i/bell -->
@@ -82,113 +83,6 @@
                             </svg>
                             <span class="badge bg-red badge-sm">{{ $jumlahPemesanan }}</span>
                         </a>
-                        {{-- <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-end dropdown-menu-card">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h3 class="card-title">Last updates</h3>
-                                </div>
-                                <div class="list-group list-group-flush list-group-hoverable">
-                                    <div class="list-group-item">
-                                        <div class="row align-items-center">
-                                            <div class="col-auto"><span
-                                                    class="status-dot status-dot-animated bg-red d-block"></span></div>
-                                            <div class="col text-truncate">
-                                                <a href="#" class="text-body d-block">Example 1</a>
-                                                <div class="d-block text-muted text-truncate mt-n1">
-                                                    Change deprecated html tags to text decoration classes (#29604)
-                                                </div>
-                                            </div>
-                                            <div class="col-auto">
-                                                <a href="#" class="list-group-item-actions">
-                                                    <!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon text-muted"
-                                                        width="24" height="24" viewBox="0 0 24 24"
-                                                        stroke-width="2" stroke="currentColor" fill="none"
-                                                        stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                        <path
-                                                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="list-group-item">
-                                        <div class="row align-items-center">
-                                            <div class="col-auto"><span class="status-dot d-block"></span></div>
-                                            <div class="col text-truncate">
-                                                <a href="#" class="text-body d-block">Example 2</a>
-                                                <div class="d-block text-muted text-truncate mt-n1">
-                                                    justify-content:between ⇒ justify-content:space-between (#29734)
-                                                </div>
-                                            </div>
-                                            <div class="col-auto">
-                                                <a href="#" class="list-group-item-actions show">
-                                                    <!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon text-yellow"
-                                                        width="24" height="24" viewBox="0 0 24 24"
-                                                        stroke-width="2" stroke="currentColor" fill="none"
-                                                        stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                        <path
-                                                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="list-group-item">
-                                        <div class="row align-items-center">
-                                            <div class="col-auto"><span class="status-dot d-block"></span></div>
-                                            <div class="col text-truncate">
-                                                <a href="#" class="text-body d-block">Example 3</a>
-                                                <div class="d-block text-muted text-truncate mt-n1">
-                                                    Update change-version.js (#29736)
-                                                </div>
-                                            </div>
-                                            <div class="col-auto">
-                                                <a href="#" class="list-group-item-actions">
-                                                    <!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon text-muted"
-                                                        width="24" height="24" viewBox="0 0 24 24"
-                                                        stroke-width="2" stroke="currentColor" fill="none"
-                                                        stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                        <path
-                                                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="list-group-item">
-                                        <div class="row align-items-center">
-                                            <div class="col-auto"><span
-                                                    class="status-dot status-dot-animated bg-green d-block"></span></div>
-                                            <div class="col text-truncate">
-                                                <a href="#" class="text-body d-block">Example 4</a>
-                                                <div class="d-block text-muted text-truncate mt-n1">
-                                                    Regenerate package-lock.json (#29730)
-                                                </div>
-                                            </div>
-                                            <div class="col-auto">
-                                                <a href="#" class="list-group-item-actions">
-                                                    <!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon text-muted"
-                                                        width="24" height="24" viewBox="0 0 24 24"
-                                                        stroke-width="2" stroke="currentColor" fill="none"
-                                                        stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                        <path
-                                                            d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> --}}
                     </div>
                 </div>
                 {{-- gawe gambar profile --}}
@@ -204,102 +98,3 @@
                         </span>
                         <div class="d-none d-xl-block ps-2">
                             <div>{{ Auth::user()->name }}</div>
-                            <div class="mt-1 small text-muted">{{ Auth::user()->roles->pluck('name')->implode(', ') }}
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                        {{-- <a href="#" class="dropdown-item">Status</a> --}}
-                        <a href="{{ route('profile.index') }}" class="dropdown-item">Profile Saya</a>
-                        <a href="#" class="dropdown-item">Laporkan Bug</a>
-                        {{-- <a href="#" class="dropdown-item">Feedback</a> --}}
-                        <div class="dropdown-divider"></div>
-                        {{-- <a href="./settings.html" class="dropdown-item">Settings</a> --}}
-                        {{-- gawe logout --}}
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}</a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
-                </div>
-            </div>
-        @endrole
-
-    </div>
-</header>
-{{-- navbar --}}
-<header class="navbar-expand-md">
-    <div class="collapse navbar-collapse" id="navbar-menu">
-        <div class="navbar">
-            <div class="container-xl">
-                <ul class="navbar-nav">
-                    <li class="nav-item {{ Route::is('supermarket.index', 'supermarket.keranjang.index', 'supermarket.pesanan.index') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('supermarket.index') }}">
-                            <span
-                                class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                                <i class="fa-solid fa-home"></i>
-                            </span>
-                            <span class="nav-link-title">
-                                Beranda
-                            </span>
-                        </a>
-                    </li>
-                    {{-- penyimpanan --}}
-                    {{-- <li
-                        class="nav-item dropdown {{ Route::is('produk.index', 'kategori.index', 'supplier.index', 'produk.show', 'kategori.show', 'supplier.show') ? 'active' : '' }}">
-                        <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
-                            data-bs-auto-close="outside" role="button" aria-expanded="false">
-                            <span
-                                class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                                <i class="fa-solid fa-box"></i>
-                            </span>
-                            <span class="nav-link-title">
-                                Penyimpanan
-                            </span>
-                        </a>
-                        <div class="dropdown-menu">
-                            <div class="dropdown-menu-columns">
-                                <div class="dropdown-menu-column">
-                                    <a class="dropdown-item {{ Route::is('produk.index', 'produk.show') ? 'active' : '' }}"
-                                        href="{{ route('produk.index') }}">
-                                        Produk
-                                    </a>
-                                    <a class="dropdown-item {{ Route::is('kategori.index', 'kategori.show') ? 'active' : '' }}"
-                                        href="{{ route('kategori.index') }}">
-                                        Kategori
-                                    </a>
-                                    <a class="dropdown-item {{ Route::is('supplier.index', 'supplier.show') ? 'active' : '' }}"
-                                        href="{{ Route('supplier.index') }}">
-                                        Supplier
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </li> --}}
-                </ul>
-                <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
-                    <form action="./" method="get" autocomplete="off" novalidate>
-                        {{-- <div class="input-icon">
-                            <span class="input-icon-addon">
-                                <!-- Download SVG icon from http://tabler-icons.io/i/search -->
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                    stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
-                                    <path d="M21 21l-6 -6" />
-                                </svg>
-                            </span>
-                            <input type="text" value="" class="form-control" placeholder="Search…"
-                                aria-label="Search in website">
-                        </div> --}}
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
