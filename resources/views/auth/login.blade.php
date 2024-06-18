8436bde061faaa46ef22df56a2148fce7026c4c8
badfellas <deruanggoro26@gmail.com>
2024-06-19 01:31:50 +0700

    fix show password login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,19 +51,10 @@
                                     @enderror
 
                                     <span class="input-group-text">
-                                        <a href="#" class="link-secondary show-password" title="Show password"
-                                            data-bs-toggle="tooltip">
-                                            <!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                                                <path
-                                                    d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                                            </svg>
-                                        </a>
-                                    </span>
+                                <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"
+                                    onclick="togglePassword()">
+                                </a>
+                            </span>
                                 </div>
                             </div>
                             <div class="mb-2">
@@ -112,7 +103,7 @@
             </div>
         </div>
     </div>
-    {{-- show password --}}
+    <!-- {{-- show password --}}
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             const showPasswordButton = document.querySelector('.show-password');
@@ -127,5 +118,5 @@
                 }
             });
         });
-    </script>
+    </script> -->
 @endsection
